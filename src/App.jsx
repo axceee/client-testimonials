99eb701e608f385045ab2a961f19eace12afcfd3
axceee <akshit14022007@gmail.com>
2025-02-24 15:31:50 +0530

    e

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import TestimonialCard from './components/TestimonialCard'
+
+function App() {
+ 
+
+  return (
+    <>
+      <TestimonialCard person={{name:['Emily Watson','Peter Parker'],remark:['good','meh']}}/>
+        
+    </>
+  )
+}
+
+export default App
